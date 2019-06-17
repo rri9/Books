197e4c7769d5b43c6edb7c4ddd667c693e79d230
rri9 <rri9@mail.ru>
2019-06-17 14:16:24 +0300

    Add Books project from c:/CS/Books

---
+++ Books/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Books {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.booksDataSet = new Books.BooksDataSet();
+            this.departmentsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.departmentsTableAdapter = new Books.BooksDataSetTableAdapters.DepartmentsTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.departmentsLiteratureBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.literatureTableAdapter = new Books.BooksDataSetTableAdapters.LiteratureTableAdapter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.названиеDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ответственныйDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.стеллажDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.кодDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.авторDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.названиеDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ценаDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.отделDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox_Search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_Discount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentsLiteratureBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn,
+            this.названиеDataGridViewTextBoxColumn,
+            this.ответственныйDataGridViewTextBoxColumn,
+            this.стеллажDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.departmentsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 229);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // booksDataSet
+            // 
+            this.booksDataSet.DataSetName = "BooksDataSet";
+            this.booksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // departmentsBindingSource
+            // 
+            this.departmentsBindingSource.DataMember = "Departments";
+            this.departmentsBindingSource.DataSource = this.booksDataSet;
+            // 
+            // departmentsTableAdapter
+            // 
+            this.departmentsTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.кодDataGridViewTextBoxColumn1,
+            this.авторDataGridViewTextBoxColumn,
+            this.названиеDataGridViewTextBoxColumn1,
+            this.ценаDataGridViewTextBoxColumn,
+            this.отделDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.departmentsLiteratureBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(13, 270);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(644, 232);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.SelectionChanged += new System.EventHandler(this.dataGridView2_SelectionChanged);
+            // 
+            // departmentsLiteratureBindingSource
+            // 
+            this.departmentsLiteratureBindingSource.DataMember = "DepartmentsLiterature";
+            this.departmentsLiteratureBindingSource.DataSource = this.departmentsBindingSource;
+            // 
+            // literatureTableAdapter
+            // 
+            this.literatureTableAdapter.ClearBeforeFill = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // кодDataGridViewTextBoxColumn
+            // 
+            this.кодDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
+            this.кодDataGridViewTextBoxColumn.Width = 51;
+            // 
+            // названиеDataGridViewTextBoxColumn
+            // 
+            this.названиеDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.названиеDataGridViewTextBoxColumn.DataPropertyName = "Название";
+            this.названиеDataGridViewTextBoxColumn.HeaderText = "Название";
+            this.названиеDataGridViewTextBoxColumn.Name = "названиеDataGridViewTextBoxColumn";
+            this.названиеDataGridViewTextBoxColumn.Width = 82;
+            // 
+            // ответственныйDataGridViewTextBoxColumn
+            // 
+            this.ответственныйDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ответственныйDataGridViewTextBoxColumn.DataPropertyName = "Ответственный";
+            this.ответственныйDataGridViewTextBoxColumn.HeaderText = "Ответственный";
+            this.ответственныйDataGridViewTextBoxColumn.Name = "ответственныйDataGridViewTextBoxColumn";
+            this.ответственныйDataGridViewTextBoxColumn.Width = 111;
+            // 
+            // стеллажDataGridViewTextBoxColumn
+            // 
+            this.стеллажDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.стеллажDataGridViewTextBoxColumn.DataPropertyName = "Стеллаж";
+            this.стеллажDataGridViewTextBoxColumn.HeaderText = "Стеллаж";
+            this.стеллажDataGridViewTextBoxColumn.Name = "стеллажDataGridViewTextBoxColumn";
+            this.стеллажDataGridViewTextBoxColumn.Width = 76;
+            // 
+            // кодDataGridViewTextBoxColumn1
+            // 
+            this.кодDataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.кодDataGridViewTextBoxColumn1.DataPropertyName = "Код";
+            this.кодDataGridViewTextBoxColumn1.HeaderText = "Код";
+            this.кодDataGridViewTextBoxColumn1.Name = "кодDataGridViewTextBoxColumn1";
+            this.кодDataGridViewTextBoxColumn1.Width = 51;
+            // 
+            // авторDataGridViewTextBoxColumn
+            // 
+            this.авторDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.авторDataGridViewTextBoxColumn.DataPropertyName = "Автор";
+            this.авторDataGridViewTextBoxColumn.HeaderText = "Автор";
+            this.авторDataGridViewTextBoxColumn.Name = "авторDataGridViewTextBoxColumn";
+            this.авторDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // названиеDataGridViewTextBoxColumn1
+            // 
+            this.названиеDataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.названиеDataGridViewTextBoxColumn1.DataPropertyName = "Название";
+            this.названиеDataGridViewTextBoxColumn1.HeaderText = "Название";
+            this.названиеDataGridViewTextBoxColumn1.Name = "названиеDataGridViewTextBoxColumn1";
+            this.названиеDataGridViewTextBoxColumn1.Width = 150;
+            // 
+            // ценаDataGridViewTextBoxColumn
+            // 
+            this.ценаDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.ценаDataGridViewTextBoxColumn.DataPropertyName = "Цена";
+            this.ценаDataGridViewTextBoxColumn.HeaderText = "Цена";
+            this.ценаDataGridViewTextBoxColumn.Name = "ценаDataGridViewTextBoxColumn";
+            this.ценаDataGridViewTextBoxColumn.Width = 58;
+            // 
+            // отделDataGridViewTextBoxColumn
+            // 
+            this.отделDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.отделDataGridViewTextBoxColumn.DataPropertyName = "Отдел";
+            this.отделDataGridViewTextBoxColumn.HeaderText = "Отдел";
+            this.отделDataGridViewTextBoxColumn.Name = "отделDataGridViewTextBoxColumn";
+            this.отделDataGridViewTextBoxColumn.Width = 63;
+            // 
+            // textBox_Search
+            // 
+            this.textBox_Search.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox_Search.Location = new System.Drawing.Point(498, 50);
+            this.textBox_Search.Name = "textBox_Search";
+            this.textBox_Search.Size = new System.Drawing.Size(162, 20);
+            this.textBox_Search.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(495, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Поиск";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(495, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Цена со скидкой (5%):";
+            // 
+            // label_Discount
+            // 
+            this.label_Discount.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label_Discount.Location = new System.Drawing.Point(514, 127);
+            this.label_Discount.Name = "label_Discount";
+            this.label_Discount.Size = new System.Drawing.Size(75, 15);
+            this.label_Discount.TabIndex = 6;
+            this.label_Discount.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 550);
+            this.Controls.Add(this.label_Discount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Search);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentsLiteratureBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BooksDataSet booksDataSet;
+        private System.Windows.Forms.BindingSource departmentsBindingSource;
+        private BooksDataSetTableAdapters.DepartmentsTableAdapter departmentsTableAdapter;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.BindingSource departmentsLiteratureBindingSource;
+        private BooksDataSetTableAdapters.LiteratureTableAdapter literatureTableAdapter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn названиеDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ответственныйDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn стеллажDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn авторDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn названиеDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ценаDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn отделDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox textBox_Search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_Discount;
+    }
+}
+
---
+++ Books/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Books {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "booksDataSet.Literature". При необходимости она может быть перемещена или удалена.
+            this.literatureTableAdapter.Fill(this.booksDataSet.Literature);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "booksDataSet.Departments". При необходимости она может быть перемещена или удалена.
+            this.departmentsTableAdapter.Fill(this.booksDataSet.Departments);
+
+        }
+
+        private void Form1_Reload() {
+            this.literatureTableAdapter.Fill(this.booksDataSet.Literature);
+            this.departmentsTableAdapter.Fill(this.booksDataSet.Departments);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            this.departmentsTableAdapter.Update(this.booksDataSet.Departments);
+            this.literatureTableAdapter.Update(this.booksDataSet.Literature);
+            this.Form1_Reload();
+        }
+
+        private void dataGridView2_SelectionChanged(object sender, EventArgs e) {
+            if (dataGridView2.CurrentRow == null) {
+                label_Discount.Text = "...";
+            }
+            else {
+                //string s = (string)dataGridView2.CurrentRow.Cells[3].Value * 0.95;
+
+                object CurBookPrice = dataGridView2.CurrentRow.Cells[3].Value;
+                /*int CurRow = dataGridView2.CurrentCell.RowIndex;
+                int CurBookPrice = dataGridView2.Rows[CurRow].Cells[3].Value;*/
+                double DiscountedPrice = Convert.ToInt32(CurBookPrice) * 0.95;
+                label_Discount.Text = DiscountedPrice.ToString();
+            }
+        }
+    }
+}
