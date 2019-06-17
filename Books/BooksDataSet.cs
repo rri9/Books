b57ff0e7b387d9e5c3c40a1dda156232190a2069
rri9 <rri9@mail.ru>
2019-06-17 22:54:48 +0300

    Add search functional 2

---
+++ Books/BooksDataSet.cs
@@ -0,0 +1,63 @@
+﻿namespace Books
+{
+
+
+    public partial class BooksDataSet
+    {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+namespace Books {
+    
+    
+    public partial class BooksDataSet {
+    }
+}
+
+namespace Books.BooksDataSetTableAdapters {
+    
+    
+    public partial class LiteratureTableAdapter {
+    }
+}
